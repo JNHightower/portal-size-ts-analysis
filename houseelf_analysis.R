6c007d47b914abe13f2cc4749db360f307ab367c
Ethan White <ethan@weecology.org>
2015-10-30 13:08:58 -0400

    Add size class function

--- houseelf_analysis.R
+++ houseelf_analysis.R
@@ -6,3 +6,9 @@ get_data <- function(){
   data <- read.csv("houseelf_earlength_dna_data.csv")
   return(data)
 }
+
+gc_content <- function(seq){
+   #Calculate the GC-content for one or more sequences
+   ear_lengths <- ifelse(seq > 10, "large", "small")
+   return(ear_lengths)
+}
